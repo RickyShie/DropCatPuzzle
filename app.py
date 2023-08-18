b48b0ff54638bbb1ba7400180366bac4e6ec0caa
Ricky Shie <rickyshie0724@gmail.com>
2023-08-18 23:54:22 +0900

    # Implement dropping functionality

--- app.py
+++ app.py
@@ -57,16 +57,23 @@ class App(tk.Tk):
                                      self.cursor_y * 72 + 60,
                                      image=self.cursor_image,
                                      tags="CURSOR")
-            print(f"Cursor at x: {self.cursor_x} y: {self.cursor_y}")
 
     def mouse_press(self, event=None):
         self.is_mouse_pressed = 1
-        print(f'Mouse has been pressed at x: {self.mouse_x}, y: {self.mouse_y}')
-        self.canvas.create_image(self.cursor_x * 72 + 60,
-                                 self.cursor_y * 72 + 60,
-                                 image=self.current_cat_image)
-
+        self.drop_cat(self.cursor_x, self.cursor_y, self.current_cat_image)
         self.generate_random_cat_image()
+
+    def drop_cat(self, x, y, image):
+        next_y = y + 1
+        if next_y < 10 and not self.is_cell_occupied(x, next_y):
+            self.canvas.delete(f"CAT_{x}_{y}")
+            self.canvas.create_image(x * 72 + 60, next_y * 72 + 60, image=image, tags=f"CAT_{x}_{next_y}")
+            self.after(500, lambda: self.drop_cat(x, next_y, image))  # 1000 milliseconds = 1 second
+        else:
+            self.canvas.create_image(x * 72 + 60, y * 72 + 60, image=image, tags=f"CAT_{x}_{y}")
+
+    def is_cell_occupied(self, x, y):
+        return bool(self.canvas.find_withtag(f"CAT_{x}_{y}"))
 
     def mouse_release(self):
         self.is_mouse_pressed = 0
