ee497cd94e579059036e3403908cd0a1548843ef
christine <rickyshie0724@gmail.com>
2023-08-19 10:32:44 +0900

    Create Cat class

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from os import listdir
 import random
 
 
+class Cat():
+    def __init__(self, color, image_obj, x, y):
+        self.color = color
+        self.image_obj = image_obj
+        self.x_cord = x
+        self.y_cord = y
+
+
 class App(tk.Tk):
 
     def __init__(self):
@@ -18,10 +26,11 @@ class App(tk.Tk):
         self.red_cat_image = tk.PhotoImage(file="./red_cat.png")
         self.yellow_cat_image = tk.PhotoImage(file="./yellow_cat.png")
         self.cat_paw_image = tk.PhotoImage(file="./cat_paw.png")
-        self.cat_image_list = [tk.PhotoImage(file=file) for file in listdir() if file.endswith('_cat.png')]
+        self.cat_image_list = [tk.PhotoImage(
+            file=file) for file in listdir() if file.endswith('_cat.png')]
 
         # Initialize a board
-        self.board = [[None for _ in range(10)] for _ in range(8)]
+        self.board = [[None for _ in range(8)] for _ in range(10)]
 
         # Mouse coordinates
         self.mouse_x = 0
@@ -61,20 +70,33 @@ class App(tk.Tk):
                                      self.cursor_y * 72 + 60,
                                      image=self.cursor_image,
                                      tags="CURSOR")
+            print(f"Selected cell x: {self.cursor_x} y: {self.cursor_y}")
 
     def mouse_press(self, event=None):
         self.is_mouse_pressed = 1
-        self.drop_cat(self.cursor_x, self.cursor_y, self.current_cat_image)
-        self.generate_random_cat_image()
+        # self.drop_cat(self.cursor_x, self.cursor_y, self.current_cat_image)
+        # self.generate_random_cat_image()
+        cat = Cat('red', self.red_cat_image, self.cursor_x, self.cursor_y)
+        # cat = self.canvas.create_image(self.cursor_x * 72 + 60,
+        #                                self.cursor_y * 72 + 60,
+        #                                image=self.red_cat_image,
+        #                                tags="RED")
+        print(f"cat: {cat}")
+        print(
+            f'A cat has been generated at x: {self.cursor_x} y: {self.cursor_y}')
+        self.board[self.cursor_x][self.cursor_y] = cat
+        print(self.board)
 
     def drop_cat(self, x, y, image):
         next_y = y + 1
-        if next_y < 10 :
+        if next_y < 10:
             self.canvas.delete(f"CAT_{x}_{y}")
-            self.canvas.create_image(x * 72 + 60, next_y * 72 + 60, image=image, tags=f"CAT_{x}_{next_y}")
+            self.canvas.create_image(
+                x * 72 + 60, next_y * 72 + 60, image=image, tags=f"CAT_{x}_{next_y}")
             self.after(500, lambda: self.drop_cat(x, next_y, image))
         else:
-            self.canvas.create_image(x * 72 + 60, y * 72 + 60, image=image, tags=f"CAT_{x}_{y}")
+            self.canvas.create_image(
+                x * 72 + 60, y * 72 + 60, image=image, tags=f"CAT_{x}_{y}")
 
     def mouse_release(self, event=None):
         self.is_mouse_pressed = 0
@@ -91,4 +113,3 @@ app = App()
 
 
 app.mainloop()
-
